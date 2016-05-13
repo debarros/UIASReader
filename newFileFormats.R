9a51045c2ef5ba501bf34cb06ba7189c5572fb47
pj.debarros@gmail.com <pj.debarros@gmail.com>
2016-05-13 16:32:33 -0400

    Added stubs of sections to analyze new.DS.01 and new.170.01 data.frames

--- newFileFormats.R
+++ newFileFormats.R
@@ -28,7 +28,6 @@ if(is.data.frame(new.SE.02)){                 #as long as SE (the simultaneous e
   SimEnr = data.frame(NYSSIS = unique(new.SE.02$STUDENT_ID_ALT), stringsAsFactors = FALSE)   #Create a dataset with 1 row for each unique NYSSIS ID
   SimEnr$LocalEntry = as.Date(SE.local$ENTRY_DATE[match(x = SimEnr$NYSSIS, table = SE.local$STUDENT_ID_ALT)], format = "%m/%d/%Y")  #load the local and other entry dates
   SimEnr$OtherEntry = as.Date(SE.other$ENTRY_DATE[match(x = SimEnr$NYSSIS, table = SE.other$STUDENT_ID_ALT)], format = "%m/%d/%Y")
-  
   SimEnr$LocalContinuer = SimEnr$LocalEntry == as.Date("2015-07-01")   #indicate if the student was enrolled as of July 1
   SimEnr$OtherContinuer = SimEnr$OtherEntry == as.Date("2015-07-01")
   SimEnr$WhoShouldExit = "Unsure"                                          #Set the default WhoShouldExit to "Unsure"
@@ -46,4 +45,18 @@ if(is.data.frame(new.SE.02)){                 #as long as SE (the simultaneous e
 
 
 
+
+str(new.DS.01)
+# District somethings ####
+# Don't know what this is.  It seems to show when a student moved from District A in one year to district B in another year.
+
+
+
+
+
+
+
+str(new.170.01)
+# False Transfers with 170 exit code ####
+#Don't know what this is.  It seems to show when a student has two enrollments for the same district in the same year, even if they don't overlap.
 
